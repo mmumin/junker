2dde3516727e036b052242689bc2d2742ee8c903
Harshavardhan Rangan <rharshavardhan@zeomega.com>
2014-09-11 22:43:42 +0530

    fixed the setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+
 setup(name='junker',
-      version="9.8.5",
+      version="9.9.0",
       url="https://pkgadmin:pypize@packages.zeomega.org/",
       author='Integration Team',
       author_email='Jivainterface@zeomega.com',
+      packages=find_packages(),
+      include_package_data = True
       )
