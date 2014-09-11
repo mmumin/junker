07ce4c2bf5c8a66d436c234f2bfca90e883376b3
Harshavardhan Rangan <rharshavardhan@zeomega.com>
2014-09-11 20:08:15 +0530

    update11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='junker',
-      version="9.5.3",
+      version="9.6.3",
       url="https://pkgadmin:pypize@packages.zeomega.org/",
       author='Integration Team',
       author_email='Jivainterface@zeomega.com',
