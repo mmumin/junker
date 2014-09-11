538846fda504a51729b8798263249a18493acb89
Harshavardhan Rangan <rharshavardhan@zeomega.com>
2014-09-11 15:00:52 +0530

    added a setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='junker',
+      version="4.0.0",
+      url="https://pkgadmin:pypize@packages.zeomega.org/",
+      author='Integration Team',
+      author_email='Jivainterface@zeomega.com',
+      )
