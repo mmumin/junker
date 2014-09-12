fdffa856308b90c74f5767e1ed7ed0a19421ad9f
Harshavardhan Rangan <rharshavardhan@zeomega.com>
2014-09-12 23:55:58 +0600

    Automatic lazy update to 12.9.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='junker',
-    version = "12.9.8",
+    version = "12.9.9",
       url="https://pkgadmin:pypize@packages.zeomega.org/",
       author='Integration Team',
       author_email='Jivainterface@zeomega.com',
